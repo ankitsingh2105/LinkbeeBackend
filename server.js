ea4535cd922200302f008e91a29cfa8b64c31750
Ankit <ankitchauhan21500@gmail.com>
2024-12-17 22:09:10 +0530

    feature : upload images

--- server.js
+++ server.js
@@ -8,7 +8,25 @@ const connectToMongoDB = require("./connect");
 const userModel = require("./models/userModel");
 
 const cookieParser = require('cookie-parser');
-const multer = require("multer");
+
+// Image uploading setup
+const Multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+    cloud_name: 'dhs2koq4i', 
+    api_key: '352246637179152',
+    api_secret: "qV_JdoxbrjMtQYeTEZWVYPdFqXU",
+});
+
+console.log("opo :: ", process.env.cloudinary_apiKey);
+
+async function handleUpload(file) {
+    const res = await cloudinary.uploader.upload(file, {
+        resource_type: "auto",
+    });
+    return res;
+}
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,44 +38,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// connectToMongoDB(" ");
-
 connectToMongoDB(process.env.MONGO_URL);
 console.log(process.env.MONGO_URL, process.env.PORT);
-// origin: "http://localhost:5173",
 
 app.use(cors({
-    origin: "https://linkbeemern.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true
 }));
-// app.set("trust proxy", 1);
 
+const storage = Multer.memoryStorage();
+const upload = Multer({ storage });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, '../FrontEnd/src/Components/User/ProfileImages')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now();
-        cb(null, uniqueSuffix + file.originalname)
-    }
-})
-const upload = multer({ storage });
-
-app.post('/upload', upload.single('avatar'), async function (req, res, next) {
+app.post("/upload", upload.single("avatar"), async (req, res) => {
+    console.log("bc" , req.file);
+    console.log(req.body);
+    const {userID} = req.body;
     try {
-        const userID = req.body.userID;
-        const imageName = req.file.filename;
+        const b64 = Buffer.from(req.file.buffer).toString("base64");
+        let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+        const cldRes = await handleUpload(dataURI);
+        console.log("shit:: ", cldRes);
         await userModel.findOneAndUpdate({ userID }, {
-            "imageUrl": imageName
+            "imageUrl": cldRes.secure_url,
         });
-        res.send("uploaded successsfully");
-    }
-    catch (error) {
+        res.status(200).send("Image uploaded successfully");
+    } catch (error) {
+        console.log("opo :: ", process.env.cloudinary_apiKey);
         console.log(error);
+        res.send({
+            message: error.message,
+        });
     }
-
-})
+}); 
 
 app.use("/login", loginRouter);
 app.use("/signup", signupRouter);
@@ -66,13 +78,13 @@ app.use("/user", userRoute);
 // Static file serving
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.get("/", (req, response) => {
-    response.send("Your backend is live!!!!");
-})
-app.get("/shit", (req, response) => {
+app.use("/", (req, response) => {
     response.send("Your backend is not live");
-})
+});
+app.use("/shit", (req, response) => {
+    response.send("Your backend not live");
+});
 
 app.listen(PORT, () => {
     console.log("listening to the serve", PORT);
-}); 
+});
